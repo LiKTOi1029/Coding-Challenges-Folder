7e26197c507de0d5b3adb8ef6d29bc5c498c5d93
LiKTOi1029 <testingboy5689@gmail.com>
2025-01-18 23:31:09 +0330

    adding options menu to palindromechecker.lua for easier usability

--- 2024/December 22/palindromeChecker.lua
+++ 2024/December 22/palindromeChecker.lua
@@ -1,9 +1,11 @@
-io.write("Input a string: ")
-local choice = string.lower(io.read())
-local choiceGSubbed = string.gsub(choice, " ", "")
-local choiceReversed = string.reverse(choiceGSubbed)
-if choiceReversed == choiceGSubbed then
-    print(true)
-else
-    print(false)
-end
+repeat
+	io.write(">>EXIT to exit the script. Input a string: ")
+	local choice = string.lower(io.read())
+	local choiceGSubbed = string.gsub(choice, " ", "")
+	local choiceReversed = string.reverse(choiceGSubbed)
+	if choiceReversed == choiceGSubbed then
+		print(true)
+	else
+		print(false)
+	end
+until choice == "EXIT"
