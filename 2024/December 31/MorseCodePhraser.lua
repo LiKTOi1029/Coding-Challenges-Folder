027f46ff8626f4911338870266d7bd3fec9b9202
LiKTOi1029 <testingboy5689@gmail.com>
2025-01-19 08:05:18 +0330

    adding key value pairs to morse code

--- 2024/December 31/MorseCodePhraser.lua
+++ 2024/December 31/MorseCodePhraser.lua
@@ -1,10 +1,42 @@
--- I have redone this with my own code.
-function __MAIN__(input)
+-- I have redone this with my own code and expanded on it a bit.
+function Translator(input,replacements)
 	
 end
 repeat
-	local MorseTable = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--..","-.-.-.","..--..",".--.-.","-.-.--",".----.","--..--",".-.-"}
-	io.write(">>Type in morse code to translate into English. Please refer to Guide.txt\n>>Type EXIT to exit the script.\n")
+	local MorseTable = {["a"] = ".-",
+						["b"] = "-...",
+						["c"] = "-.-.",
+						["d"] = "-..",
+						["e"] = ".",
+						["f"] = "..-.",
+						["g"] = "--.",
+						["h"] = "....",
+						["i"] = "..",
+						["j"] = ".---",
+						["k"] = "-.-",
+						["l"] = ".-..",
+						["m"] = "--",
+						["n"] = "-.",
+						["o"] = "---",
+						["p"] = ".--.",
+						["q"] = "--.-",
+						["r"] = ".-.",
+						["s"] = "...",
+						["t"] = "-",
+						["u"] = "..-",
+						["v"] = "...-",
+						["w"] = ".--",
+						["x"] = "-..-",
+						["y"] = "-.--",
+						["z"] = "--..",
+						["."] = "-.-.-.",
+						["?"] = "..--..",
+						["@"] = ".--.-.",
+						["!"] = "-.-.--",
+						["'"] = ".----.",
+						[","] = "--..--",
+						["\n"] = ".-.-"}
+	io.write(">>Please type GO to convert files. Please refer to Guide.txt\n>>Type EXIT to exit the script.\n")
 	local input = io.read("*l"):gsub("\n","")
-	if input ~= "EXIT" then __MAIN__(input) end
+	if input ~= "EXIT" then Translator(input,MorseTable) end
 until input == "EXIT"
