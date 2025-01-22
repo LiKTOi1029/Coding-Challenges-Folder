be609a159953b53f052e239d0421d0191784cd04
LiKTOi1029 <testingboy5689@gmail.com>
2025-01-22 17:16:48 +0330

    finished english to morse translator

--- 2024/December 31/MorseCodePhraser.lua
+++ 2024/December 31/MorseCodePhraser.lua
@@ -1,10 +1,22 @@
+MorseTable, PermaTable = {["a"] = ".-",["b"] = "-...",["c"] = "-.-.",["d"] = "-..",["e"] = ".",["f"] = "..-.",["g"] = "--.",["h"] = "....",["i"] = "..",["j"] = ".---",["k"] = "-.-",["l"] = ".-..",["m"] = "--",["n"] = "-.",["o"] = "---",["p"] = ".--.",["q"] = "--.-",["r"] = ".-.",["s"] = "...",["t"] = "-",["u"] = "..-",["v"] = "...-",["w"] = ".--",["x"] = "-..-",["y"] = "-.--",["z"] = "--..",["."] = ".-.-.-",["?"] = "..--..",["@"] = ".--.-.",["!"] = "-.-.--",["'"] = ".----.",[","] = "--..--",["\n"] = ".-.-",[" "] = "/",[":"] = "---...",[";"] = "-.-.-.",}, {[">||<"] = "\n",}
+ReverseTable = {}; for key, value in pairs(MorseTable) do ReverseTable[value] = key; print(value.. " "..value2) end
 function Determiner(userchoice, input, output)
-	local firstline = input:read("*l")
-	if userchoice == "GO" then --[[important code--]] end
-	if string.find(firstline,"%a+") then EnglishTranslator(firstline, input, output) elseif string.find(firstline,"[-.\]") then MorseTranslator(firstline, input, output) end
+	local firstline
+	if userchoice == "GO" then firstline = input:read("*l") else firstline = userchoice end
+	if string.find(firstline,"%a+") then EnglishTranslator(firstline, input, output) elseif string.find(firstline,"[-.\]") then MorseTranslator(firstline, input, output) else print("This text is invalid. If you believe this is a mistake please create an issue about it at https://github.com/LiKTOi1029/Coding-Challenges-Folder/issues") end
 end
 function EnglishTranslator(firstline, input, output)
-	
+	local iteratedString = {}
+	for num1 = 1, firstline:len(), 1 do
+		table.insert(iteratedString, MorseTable[firstline:sub(num1,num1)])
+		print(iteratedString[num1])
+	end
+	if input and output then for num2, str in input:lines() do 
+		table.insert(iteratedString, MorseTable[str])
+		end
+	end
+	local result = table.concat(iteratedString, " ")
+	return result
 end
 function MorseTranslator(firstline, input, output)
 	
