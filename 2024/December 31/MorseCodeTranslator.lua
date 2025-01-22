99c5bf468eceb774593e5886dc2a53ff35623def
LiKTOi1029 <testingboy5689@gmail.com>
2025-01-22 18:14:45 +0330

    minor tweaks, qol changes

--- 2024/December 31/MorseCodeTranslator.lua
+++ 2024/December 31/MorseCodeTranslator.lua
@@ -2,15 +2,13 @@ MorseTable, PermaTable = {["a"] = ".-",["b"] = "-...",["c"] = "-.-.",["d"] = "-.
 ReverseTable = {}; for key, value in pairs(MorseTable) do ReverseTable[value] = key; print(value.. " "..key) end
 function Determiner(input, output, userchoice)
 	local firstline
-	if userchoice == "GO" then firstline = input:read("*l") else firstline = userchoice end
-	if string.find(firstline,"%a+") then EnglishTranslator(firstline, input, output) elseif string.find(firstline,"[-.\]") then MorseTranslator(firstline, input, output) else print("This text is invalid. If you believe this is a mistake please create an issue about it at https://github.com/LiKTOi1029/Coding-Challenges-Folder/issues") end
+	if userchoice == "go" then firstline = input:read("*l") else firstline = userchoice end
+	if string.find(firstline,"%a+") then return EnglishTranslator(firstline, input, output) elseif string.find(firstline,"[-.\]") then return MorseTranslator(firstline, input, output) else return "This text is invalid. If you believe this is a mistake please create an issue about it at https://github.com/LiKTOi1029/Coding-Challenges-Folder/issues" end
 end
 function EnglishTranslator(firstline, input, output)
 	local iteratedString = {}
-	if firstline ~= "GO" then
-		for num1 = 1, firstline:len(), 1 do
-			table.insert(iteratedString, MorseTable[string.lower(firstline:sub(num1,num1))])
-		end
+	for num1 = 1, firstline:len(), 1 do
+		table.insert(iteratedString, MorseTable[string.lower(firstline:sub(num1,num1))])
 	end
 	if input and output and firstline == "GO" then for num2, str in input:lines() do
 		print(tostring(str))
@@ -22,19 +20,21 @@ function EnglishTranslator(firstline, input, output)
 		output:write(result);output:close();input:close()
 		return result
 	end
-	print(firstline)
-	return tostring(firstline)
+	local result = table.concat(iteratedString, " ")
+	return result
 end
 function MorseTranslator(firstline, input, output)
 	
 end
 function ChoiceMenu()
-	repeat
+	while true do
 		io.flush()
 		print("Type anything to translate. Type GO to translate input file into output file.\nType EXIT to exit the script")
 		local userchoice = io.read():gsub("\n","")
-		if userchoice == "GO" then io.write(Determiner(io.open("input.txt","r"), io.open("output.txt","w+"), string.lower(userchoice))) else io.write(Determiner(nil, nil, string.lower(userchoice))) end
-	until userchoice == "EXIT"
+		if userchoice == "GO" then io.write(Determiner(io.open("input.txt","r"), io.open("output.txt","w+"), string.lower(userchoice)))
+		elseif userchoice == "EXIT" then return nil
+		else io.write(Determiner(nil, nil, string.lower(userchoice)).."\n") end
+	end
 	io.flush()
 end
 ChoiceMenu()
