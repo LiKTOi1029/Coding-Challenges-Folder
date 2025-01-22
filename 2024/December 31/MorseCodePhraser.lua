d6e416ddeacb8d9d09d0c3134865f4bd65895482
LiKTOi1029 <testingboy5689@gmail.com>
2025-01-22 16:51:42 +0330

    completing Determiner() in new morse code translator

--- 2024/December 31/MorseCodePhraser.lua
+++ 2024/December 31/MorseCodePhraser.lua
@@ -1,17 +1,19 @@
 function Determiner(userchoice, input, output)
+	local firstline = input:read("*l")
+	if userchoice == "GO" then --[[important code--]] end
+	if string.find(firstline,"%a+") then EnglishTranslator(firstline, input, output) elseif string.find(firstline,"[-.\]") then MorseTranslator(firstline, input, output) end
+end
+function EnglishTranslator(firstline, input, output)
 	
 end
-function EnglishTranslator()
-	
-end
-function MorseTranslator()
+function MorseTranslator(firstline, input, output)
 	
 end
 function ChoiceMenu()
 	repeat
 		print("Type anything to translate. Type GO to translate input file into output file.\nType EXIT to exit the script")
 		local userchoice = io.read():gsub("\n","")
-		if userchoice == "GO" then Determiner(userchoice, io.open("input.txt","r"), io.open("output.txt","w+")) else Determiner(userchoice) end
+		if userchoice == "GO" then print(Determiner(userchoice, io.open("input.txt","r"), io.open("output.txt","w+")) else Determiner(userchoice) end)
 	until userchoice ~= "EXIT"
 end
 ChoiceMenu()
