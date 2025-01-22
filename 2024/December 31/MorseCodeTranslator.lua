d2121d63b2b93429c5d8ab68fc9186e39fcd1d86
LiKTOi1029 <testingboy5689@gmail.com>
2025-01-22 18:03:07 +0330

    making minor changes. Cleaning up

--- 2024/December 31/MorseCodeTranslator.lua
+++ 2024/December 31/MorseCodeTranslator.lua
@@ -1,7 +1,6 @@
 MorseTable, PermaTable = {["a"] = ".-",["b"] = "-...",["c"] = "-.-.",["d"] = "-..",["e"] = ".",["f"] = "..-.",["g"] = "--.",["h"] = "....",["i"] = "..",["j"] = ".---",["k"] = "-.-",["l"] = ".-..",["m"] = "--",["n"] = "-.",["o"] = "---",["p"] = ".--.",["q"] = "--.-",["r"] = ".-.",["s"] = "...",["t"] = "-",["u"] = "..-",["v"] = "...-",["w"] = ".--",["x"] = "-..-",["y"] = "-.--",["z"] = "--..",["."] = ".-.-.-",["?"] = "..--..",["@"] = ".--.-.",["!"] = "-.-.--",["'"] = ".----.",[","] = "--..--",["\n"] = ".-.-",[" "] = "/",[":"] = "---...",[";"] = "-.-.-.",}, {[">||<"] = "\n",}
 ReverseTable = {}; for key, value in pairs(MorseTable) do ReverseTable[value] = key; print(value.. " "..key) end
 function Determiner(input, output, userchoice)
-	print(userchoice)
 	local firstline
 	if userchoice == "GO" then firstline = input:read("*l") else firstline = userchoice end
 	if string.find(firstline,"%a+") then EnglishTranslator(firstline, input, output) elseif string.find(firstline,"[-.\]") then MorseTranslator(firstline, input, output) else print("This text is invalid. If you believe this is a mistake please create an issue about it at https://github.com/LiKTOi1029/Coding-Challenges-Folder/issues") end
@@ -11,7 +10,6 @@ function EnglishTranslator(firstline, input, output)
 	if firstline ~= "GO" then
 		for num1 = 1, firstline:len(), 1 do
 			table.insert(iteratedString, MorseTable[string.lower(firstline:sub(num1,num1))])
-			print(iteratedString[num1])
 		end
 	end
 	if input and output and firstline == "GO" then for num2, str in input:lines() do
@@ -24,6 +22,7 @@ function EnglishTranslator(firstline, input, output)
 		output:write(result);output:close();input:close()
 		return result
 	end
+	print(firstline)
 	return tostring(firstline)
 end
 function MorseTranslator(firstline, input, output)
@@ -34,7 +33,7 @@ function ChoiceMenu()
 		io.flush()
 		print("Type anything to translate. Type GO to translate input file into output file.\nType EXIT to exit the script")
 		local userchoice = io.read():gsub("\n","")
-		if userchoice == "GO" then io.write(Determiner(io.open("input.txt","r"), io.open("output.txt","w+"), string.lower(userchoice))) else io.write(Determiner(string.lower(userchoice))) end
+		if userchoice == "GO" then io.write(Determiner(io.open("input.txt","r"), io.open("output.txt","w+"), string.lower(userchoice))) else io.write(Determiner(nil, nil, string.lower(userchoice))) end
 	until userchoice == "EXIT"
 	io.flush()
 end
