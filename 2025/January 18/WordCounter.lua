e1cde091c05bb3b7edd1d71b27aae780260545b6
LiKTOi1029 <testingboy5689@gmail.com>
2025-01-23 10:54:06 +0330

    bugfixing january 18

--- 2025/January 18/WordCounter.lua
+++ 2025/January 18/WordCounter.lua
@@ -1,10 +1,11 @@
 function string.wc(input)
 	local parser, count = "", 0
 	for num1 = 1, input:len(), 1 do
-		if input:sub(num1,num1) ~= " " then parser = parser..input:sub(num1,num1); print(parser)
-		elseif input:sub(num1,num1) == " " or num1 == input:len() then parser = ""; count = count+1; print(count.." "..parser) end
-	return count
+		if input:sub(num1,num1) ~= " " and num1 ~= input:len() then parser = parser..input:sub(num1,num1);
+		elseif input:sub(num1,num1) == " " then parser = ""; count = count+1
+		else parser = ""; count=count+1 end
 	end
+	return count
 end
 function MAIN()
 	repeat
