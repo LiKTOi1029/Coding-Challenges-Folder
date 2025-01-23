6d8d9070d3812a7f64478b0e0113c5b7a233b2de
LiKTOi1029 <testingboy5689@gmail.com>
2025-01-23 10:46:30 +0330

    finishing up January 18: bugfixing

--- 2025/January 18/WordCounter.lua
+++ 2025/January 18/WordCounter.lua
@@ -1,9 +1,15 @@
-function string.wreverser(input)
-	
-end
-function __MAIN__()
-	local choice = io.read("*l"):gsub("\n","")
-	while choice ~= "EXIT"
-	if choice ~= "EXIT" then string.wreverser(choice) end
+function string.wc(input)
+	local parser, count = "", 0
+	for num1 = 1, input:len(), 1 do
+		if input:sub(num1,num1) ~= " " then parser = parser..input:sub(num1,num1); print(parser)
+		elseif input:sub(num1,num1) == " " or num1 == input:len() then parser = ""; count = count+1; print(count.." "..parser) end
+	return count
 	end
 end
+function MAIN()
+	repeat
+	local choice = io.read("*l"):gsub("\n","")
+	if choice ~= "EXIT" then print(string.wc(tostring(choice))) end
+	until choice == "EXIT"
+end
+MAIN()
