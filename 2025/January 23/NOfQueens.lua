c19874f3e7f1dd997630a701c26bab19e16130f2
LiKTOi1029 <testingboy5689@gmail.com>
2025-01-25 00:33:10 +0330

    adding in vertical iterator. Now the script checks vertically for Queens nofqueens.lua

--- 2025/January 23/NOfQueens.lua
+++ 2025/January 23/NOfQueens.lua
@@ -1,21 +1,31 @@
+function ticker(chessboard, input, ) 
+
+end
 function chessboardbuilder(input)
 	local wholeboard = {}
 	for num1 = 1, input, 1 do
 		local newtable = {}
 		for num2 = 1, input, 1 do
-			if num2 = 1 then table.insert(newtable, "Q")
+			if num2 == 1 then table.insert(newtable, "Q")
 			else table.insert(newtable, "X") end
 			io.write(newtable[num2])
 		end
 		io.write("\n")
 		wholeboard[num1] = newtable
 	end
-	return wholeboard
+	return queens(wholeboard,input)
 end
 function queens(chessboard,input)
-	for num3, squares in ipairs(chessboard) do
-	
+	for num3, rows in ipairs(chessboard) do
+		for num4, squares in ipairs(rows) do
+			squares = "Q"
+			for num5, _2 in ipairs(chessboard) do
+				if rows[num4] == "Q" then break end
+			end
+			squares = "X"
+		end
 	end
+	return chessboard
 end
 repeat
 	io.write("Please input an integer! Input EXIT to exit the script\n")
