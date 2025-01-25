f552ed8573fce2cb5a3b1649e0a7f284b7e8b6d6
LiKTOi1029 <testingboy5689@gmail.com>
2025-01-25 11:57:53 +0330

    commenting nofqueens.lua preparing to ask for help lmao

--- 2025/January 23/NOfQueens.lua
+++ 2025/January 23/NOfQueens.lua
@@ -1,3 +1,7 @@
+function table.replace(tab, originalLocation, replacement)
+table.remove(tab, originalLocation); table.insert(tab, originalLocation, replacement)
+return tab
+end
 function chessboardbuilder(input)
 	local wholeboard = {}
 	for num1 = 1, input, 1 do
@@ -14,25 +18,25 @@ function chessboardbuilder(input)
 end
 function queens(chessboard,input)
 	local num6, verticalAxis = 1, nil
-	for num3, rows in ipairs(chessboard) do
-		for num4, squares in ipairs(rows) do
-			squares = "Q"
-			for num5 = num3, 1, -1 do
+	for rank, rows in ipairs(chessboard) do -- This iterates over every single horizontal row on the chessboard
+		for file, squares in ipairs(rows) do -- This iterates over every single square in every horizontal row (I names the int value "file" because it is really just iterating through the vertical columns or "files" on the chessboard
+			table.replace(rows, rows[file], "Q") -- This is a custom function that replaces one object in a table with another
+			for num5 = rank, 1, -1 do -- This iterates over every rank starting from the position of the current queen upwards. This mainly just checks if there is a queen in line of sight of this particular queen
 				verticalAxis = chessboard[num5]
-				if verticalAxis[num4] == "Q" then break
-				elseif verticalAxis[num4+num6] == "Q" then break
-				elseif verticalAxis[num4-num6] == "Q" then break end
+				if verticalAxis[file] == "Q" then break --Queens are represented with Q
+				elseif verticalAxis[file+num6] == "Q" then break
+				elseif verticalAxis[file-num6] == "Q" then break end
 				num6=num6+1
 			end
 			num6 = 1
-			for num7 = num3, #chessboard, 1 do
+			for num7 = rank, #chessboard, 1 do
 				verticalAxis = chessboard[num7]
-				if verticalAxis[num4] == "Q" then break
-				elseif verticalAxis[num4+num6] == "Q" then break
-				elseif verticalAxis[num4-num6] == "Q" then break end
+				if verticalAxis[file] == "Q" then break
+				elseif verticalAxis[file+num6] == "Q" then break
+				elseif verticalAxis[file-num6] == "Q" then break end
 				num6=num6+1
 			end
-			if num4 < #rows then squares = "X" end
+			if file < #rows then table.replace(rows, rows[file], "X") end --Empty squares are represented with X
 		end
 	end
 	return chessboard
