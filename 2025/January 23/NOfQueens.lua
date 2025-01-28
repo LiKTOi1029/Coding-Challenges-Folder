86c6ee888b9811a6d507a4e70ae9b5d13e5301d7
LiKTOi1029 <testingboy5689@gmail.com>
2025-01-28 19:14:45 +0330

    nofqueens.lua adding the first planned function into

--- 2025/January 23/NOfQueens.lua
+++ 2025/January 23/NOfQueens.lua
@@ -2,25 +2,6 @@ function queencounter(queenpos)
 	for _, num1 in ipairs(queenpos) do if num1 ~= #queenpos then return false end end
 	return true
 end
---[[function queenchecker()
-	table.replace(rows, rows[file], "Q")
-	for num5 = rank, 1, -1 do
-		verticalAxis = chessboard[num5]
-		if verticalAxis[file] == "Q" then break
-		elseif verticalAxis[file+num6] == "Q" then break
-		elseif verticalAxis[file-num6] == "Q" then break end
-		num6=num6+1
-	end
-	num6 = 1
-	for num7 = rank, #chessboard, 1 do
-		verticalAxis = chessboard[num7]
-		if verticalAxis[file] == "Q" then break
-		elseif verticalAxis[file+num6] == "Q" then break
-		elseif verticalAxis[file-num6] == "Q" then break end
-		num6=num6+1
-	end
-	if file < #rows then table.replace(rows, rows[file], "X") end
-end--]]
 function table.replace(tab, originalLocation, replacement)
 	table.remove(tab, originalLocation); table.insert(tab, originalLocation, replacement)
 	return tab
@@ -28,7 +9,6 @@ end
 function chessboardbuilder(input)
 	local wholeboard = {}
 	for num1 = 1, input, 1 do
-		local queenpos = {}
 		local newtable = {}
 		for num2 = 1, input, 1 do
 			if num2 == 1 then table.insert(newtable, "Q")
@@ -37,17 +17,22 @@ function chessboardbuilder(input)
 		end
 		io.write("\n")
 		wholeboard[num1] = newtable
-		table.insert(queenpos, 1)
 	end
-	return queens(wholeboard,input,queenpos)
+	return PosIterator(wholeboard,input)
 end
-function queens(chessboard,input,queenpos)
-	local num6, verticalAxis, boolean1 = 1, nil
+function PosIterator(Chessboard,Input)
+	local count, retvalue, solutionsnumber = 0, 0, 0
 	repeat
-		if queenpos[num6] == input then boolean1 = queencounter(queenpos) end
-		queenpos[num6] = queenpos[num6]+1
-	until 
-	return chessboard
+		for unusedIntVariable, Row in ipairs(Chessboard) do
+			retvalue = retvalue + BoardChecker(Chessboard)
+			if Row[Input] == "Q" then count = count + 1; Row[Input], Row[1] = Row[1], Row[Input]
+			else break end
+		end
+		for positionInt, Squares in ipairs(Chessboard[count+1]) do
+			if Squares == "Q" then Chessboard[count+1][positionInt] = Chessboard[count+1][positionInt+1]; break end
+		end
+	until true == AllQueensIterated(Chessboard,Input)
+	return retValue
 end
 repeat
 	io.write("Please input an integer! Input EXIT to exit the script\n")
