d82c62e410f86b0d626526b2edf51df27d7ef839
LiKTOi1029 <testingboy5689@gmail.com>
2025-02-02 14:55:40 +0330

    TrinaryEncrypter.lua adding in conditionals and beginning work on the tabler

--- 2025/Febuary 2/TrinaryEncrypter.lua
+++ 2025/Febuary 2/TrinaryEncrypter.lua
@@ -1,5 +1,7 @@
 function Tabler(Input)
-	if Input ~= "ENCRYPT" or Input ~= "DECRYPT" then Tabler(Input, true) elseif Input == "ENCRYPT" or Input == "DECRYPT" then Tabler(Input, false) end
+	local EncryptDecryptTypeBoolean, TextTypeBoolen, InFile, OutFile
+	if Input ~= "ENCRYPT" or Input ~= "DECRYPT" then EncryptDecryptTypeBoolean = true elseif Input == "ENCRYPT" or Input == "DECRYPT" then InFile, OutFile, EncryptDecryptTypeBoolean = io.open("input.txt", "r"), io.open("output.txt", "w+"), false end
+	if Input:find("%a+") then TextTypeBoolean = true elseif Input:find("[012]") then TextTypeBoolean = false end
 	
 end
 repeat 
