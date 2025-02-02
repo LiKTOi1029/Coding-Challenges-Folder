a67d10eed6ef02c47f63298399629fb0e7dc96e3
LiKTOi1029 <testingboy5689@gmail.com>
2025-02-02 15:22:22 +0330

    trinaryencrypter.lua saving progress, moving to main device

--- 2025/Febuary 2/TrinaryEncrypter.lua
+++ 2025/Febuary 2/TrinaryEncrypter.lua
@@ -1,10 +1,17 @@
 function Tabler(Input)
-	local EncryptDecryptTypeBoolean, TextTypeBoolen, InFile, OutFile
+	local ConversionTable, EncryptDecryptTypeBoolean, TextTypeBoolen, InFile, OutFile = {}
 	if Input ~= "ENCRYPT" or Input ~= "DECRYPT" then EncryptDecryptTypeBoolean = true elseif Input == "ENCRYPT" or Input == "DECRYPT" then InFile, OutFile, EncryptDecryptTypeBoolean = io.open("input.txt", "r"), io.open("output.txt", "w+"), false end
-	if Input:find("%a+") and EncryptDecryptTypeBoolean then TextTypeBoolean = true elseif Input:find("[012]") and EncryptDecryptTypeBoolean then TextTypeBoolean = false end
-	
+	if Input:find("%a+") and EncryptDecryptTypeBoolean then TextTypeBoolean = true; break elseif Input:find("[012]") and EncryptDecryptTypeBoolean then TextTypeBoolean = false end
+	if TextTypeBoolean then
+		for num1 = 1, #Input, 1 do
+			
+			
+			
+		end
+	end
 end
 repeat 
 	io.write("ENCRYPT to encrypt, DECRYPT to decrypt, EXIT to exit.")
 	local choice = io.read("*l"):gsub("\n", "")
+	if choice ~= "EXIT" then Tabler(choice) end
 until choice == "EXIT"
