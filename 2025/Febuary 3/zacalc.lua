e6ccfbee32c26cd40ebeaa53f8f8f9d3d5cb186f
LiKTOi1029 <testingboy5689@gmail.com>
2025-02-03 11:31:35 +0330

    zacalc.lua saving progress

--- 2025/Febuary 3/zacalc.lua
+++ 2025/Febuary 3/zacalc.lua
@@ -1,11 +1,13 @@
 function Calculator(Input)
 	local ParserString, CalculationTable = "", {}
 	for num1 = 1, #Input, 1 do
-		if Input:sub(num1,num1) ~= " " then ParserString = ParserString..Input:sub(num1,num1) elseif Input:sub(num1,num1) == " " then table.insert(CalculationTable, tonumber(ParserString)); ParserString = "" end
+		ParserString = ParserString..Input:sub(num1,num1)
+		if Input:sub(num1,num1) == " " or num1 == Input:len() then ParserString:gsub(" ", "") table.insert(CalculationTable, ParserString); ParserString = "" end
+		print(ParserString.." "..#CalculationTable)
 	end
 	for num2, value in ipairs(CalculationTable) do io.write(value.." ") end
 	local result = CalculationTable[1]*(CalculationTable[2]/CalculationTable[3])*CalculationTable[4]
-	if CalculationTable[4] and CalculationTable[5] then local result = result+(math.floor(CalculationTable[4]/CalculationTable[5])) end
+	if CalculationTable[6] then result = result+(math.floor(CalculationTable[4]/CalculationTable[5])) end
 	return result
 end
 repeat
