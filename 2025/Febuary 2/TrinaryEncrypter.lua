c6375d66fd1ce1dd82ce0c2cb2a7ea0bfbe956a2
LiKTOi1029 <testingboy5689@gmail.com>
2025-02-03 06:59:02 +0330

    trinaryencrypter.lua adding debugging prints to check in/out flow

--- 2025/Febuary 2/TrinaryEncrypter.lua
+++ 2025/Febuary 2/TrinaryEncrypter.lua
@@ -1,9 +1,11 @@
 function TrinaryEncoder(ASCII)
 	local EncoderTable, IndiceDivision, IndiceMod, ASCIIResult = {}, math.floor(ASCII/3), ASCII%3
 	table.insert(EncoderTable, 1, IndiceMod)
+	print("\[INITIAL\]: These are the ascii values outputted "..ASCII.." "..IndiceDivision.." "..IndiceMod)
 	repeat
 		IndiceDivision = math.floor(IndiceDivision/3); IndiceModNew = IndiceDivision%3; ASCIIResult = IndiceDivision
-		table.insert(EncoderTable, 1, IndiceModNew)
+		if IndiceDivision > 0 then table.insert(EncoderTable, 1, IndiceModNew) end
+		print("\[UNTIL ASCII 0\]: These are the ascii values outputted "..IndiceDivision.." "..IndiceModNew)
 	until ASCIIResult == 0
 	local result = table.concat(EncoderTable)
 	return result
@@ -14,7 +16,7 @@ function Tabler(Input)
 	if Input:find("%a+") and EncryptDecryptTypeBoolean then TextTypeBoolean = true elseif Input:find("[012]") and EncryptDecryptTypeBoolean then TextTypeBoolean = false end
 	if TextTypeBoolean then
 		for num1 = 1, #Input, 1 do
-			local Indice = string.byte(Input, num1); table.insert(ConversionTable, TrinaryEncoder(Indice))
+			local Indice = string.byte(Input, num1); print("\[TABLER\]: This is the indice from Tabler. "..Indice); table.insert(ConversionTable, TrinaryEncoder(Indice))
 		end
 		return table.concat(ConversionTable, " ")
 	end
