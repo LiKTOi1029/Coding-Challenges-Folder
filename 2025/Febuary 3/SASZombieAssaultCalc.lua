45dc34bdd436a594d95073961b936f0d269484ff
LiKTOi1029 <testingboy5689@gmail.com>
2025-02-05 14:23:52 +0330

    saszombieassaultcalc.lua filling out begin() function with formula and added effect damage and time calculation

--- 2025/Febuary 3/SASZombieAssaultCalc.lua
+++ 2025/Febuary 3/SASZombieAssaultCalc.lua
@@ -2,7 +2,9 @@ tinytoml = require("tinytoml");
 Settings = tinytoml.parse("settings.toml");
 DamagePerMag, DamagePerMinute, DamagePerSecond, AllAtOnce, FileUsage, Logging, DefaultIn, DefaultOut = Settings["GLOBAL_SETTINGS"]["DamagePerMag"], Settings["GLOBAL_SETTINGS"]["DamagePerMinute"], Settings["GLOBAL_SETTINGS"]["DamagePerSecond"], Settings["GLOBAL_SETTINGS"]["AllAtOnce"],  Settings["GLOBAL_SETTINGS"]["FileUsage"], Settings["GLOBAL_SETTINGS"]["Logging"], Settings["GLOBAL_SETTINGS"]["DefaultIn"], Settings["GLOBAL_SETTINGS"]["DefaultOut"]
 function Begin(CalcTable)
-	
+	local MagDumpTime = (CalcTable[2]/CalcTable[3]); local MinuteReloadTime = (60/(MagDumpTime)+CalcTable[6])
+	local DamagePerMag = (CalcTable[1]*MagDumpTime*CalcTable[4]*CalcTable[5])
+	if CalcTable
 end
 --[[function BeginSetTableFiler()
 	
@@ -16,6 +18,8 @@ function BeginSetTableTerminal(choice)
 		io.write(">Pierce: "); pierce = io.read("*l"):gsub("\n",""); table.insert(CalcTable, pierce)
 		io.write(">Projectiles Per Shot: "); projectiles = io.read("*l"):gsub("\n",""); table.insert(CalcTable, projectiles)
 		io.write(">Reload Time: "); reloadtime = io.read("*l"):gsub("\n",""); table.insert(CalcTable, reloadtime)
+		io.write(">[OPTIONAL] Effect Damage: "); effectdmg = io.read("*l"):gsub("\n",""); table.insert(CalcTable, effectdmg)
+		io.write(">[OPTIONAL] Effect Seconds: "); effectseconds = io.read("*l"):gsub("\n",""); table.insert(CalcTable, effectseconds)
 	return Begin(CalcTable)
 	else
 		io.write(">Damage, Ammunition, RPS, Pierce, Projectiles Per Shot, Reload Time:\n"); choice = io.read("*l"):gsub("\n","")
