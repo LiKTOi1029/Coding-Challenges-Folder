2a742f3cfe648922f67a6e9b7b6446726d101f7c
LiKTOi1029 <testingboy5689@gmail.com>
2025-02-05 08:26:31 +0330

    saszombieassaultcalc.lua adding in one-by-one insertion

--- 2025/Febuary 3/SASZombieAssaultCalc.lua
+++ 2025/Febuary 3/SASZombieAssaultCalc.lua
@@ -1,4 +1,4 @@
-DamagePerMag, DamagePerMinute, DamagePerSecond = "X", "O", "X"
+DamagePerMag, DamagePerMinute, DamagePerSecond, AllAtOnce = "X", "O", "X", "O"
 function Toggler(input)
 	if input == "X" then input = "O" else input = "X" end
 	return input
@@ -6,12 +6,12 @@ end
 function WriteFile()
 	local infile, outfile, result1, result2
 	while not infile do
-		io.write("[FILER]: If no file is found it will create one manually\n[FILER]: Please give the name of the input file: ")
+		io.write(">[FILER]: If no file is found it will create one manually\n[FILER]: Please give the name of the input file: ")
 		infile = io.read("*l"):gsub("\n","")
 		if infile == " " or not infile then print("Please input a valid name"); infile = nil end
 	end
 	while not outfile do
-		io.write("[FILER]: If no file is found it will create one manually\n[FILER]: Please give the name of the output file: ")
+		io.write(">[FILER]: If no file is found it will create one manually\n[FILER]: Please give the name of the output file: ")
 		outfile = io.read("*l"):gsub("\n","")
 		if outfile == " " or not outfile then print("Please input a valid name"); outfile = nil end
 	end
@@ -20,16 +20,29 @@ function WriteFile()
 	return result1, result2
 end
 function Settings()
-	io.write("1) DamagePerMag calculation ["..DamagePerMag.."]\n2) DamagePerMinute calculation ["..DamagePerMinute.."]\n3) DamagePerSecond calculation ["..DamagePerSecond.."]\n4) Accept and return to menu\n")
+	io.write(">1) DamagePerMag calculation ["..DamagePerMag.."]\n>2) DamagePerMinute calculation ["..DamagePerMinute.."]\n>3) DamagePerSecond calculation ["..DamagePerSecond.."]\n>4) Input values one by one ["..AllAtOnce.."]\n>5) Accept and return to menu\n")
 	local choice = io.read("*l"):gsub("\n","")
 	while choice ~= "4" do
-		if choice == "1" then io.write("DamagePerMag calculation toggle")
+		if choice == "1" then io.write(">DamagePerMag calculation toggle\n"); Toggler(DamagePerMag)
+		elseif choice == "2" then io.write(">DamagePerMinute calculation toggle\n"); Toggler(DamagePerMinute)
+		elseif choice == "3" then io.write(">DamagePerSecond calculation toggle\n"); Toggler(DamagePerSecond)
+		elseif choice == "4" then io.write(">AllAtOnce toggle"); Toggler(AllAtOnce)
+		else io.write(">Changes saved\n") end
 	end
 end
-function Begin()
-
-end
+function BeginSetTable(choice)
+	local CalcTable, damage, ammunition, rps, pierce, projectiles, reloadtime = {}
+	if AllAtOnce == "X" then
+	io.write(">Damage: "); damage = io.read("*l"):gsub("\n",""); table.insert(CalcTable, damage)
+	io.write(">Ammunition: "); ammunition = io.read("*l"):gsub("\n",""); table.insert(CalcTable, ammunition)
+	io.write(">RPS: "); rps = io.read("*l"):gsub("\n",""); table.insert(CalcTable, rps)
+	io.write(">Pierce: "); pierce = io.read("*l"):gsub("\n",""); table.insert(CalcTable, pierce)
+	io.write(">Projectiles Per Shot: "); projectiles = io.read("*l"):gsub("\n",""); table.insert(CalcTable, projectiles)
+	io.write(">Reload Time: "); reloadtime = io.read("*l"):gsub("\n",""); table.insert(CalcTable, reloadtime)
+	else end
 repeat
 	io.write("1) Settings\n2)Begin\n3)EXIT\n")
 	local choice = io.read("*l"):gsub("\n","")
+	if choice == "1" then Settings()
+	elseif choice == "2" then BeginSetTable() end 
 until choice == "3"
