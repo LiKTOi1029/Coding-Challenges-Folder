b2b8a294c97e5265c2328b4b406662a0ee13daae
LiKTOi1029 <testingboy5689@gmail.com>
2025-02-18 14:18:25 +0330

    saszombieassaultcalc.lua bugfixes

--- 2025/February 3/SASZombieAssaultCalc.lua
+++ 2025/February 3/SASZombieAssaultCalc.lua
@@ -44,7 +44,7 @@ function Begin(CalcTable)
 	local MagDumpTime = (CalcTable[2]/CalcTable[3]); local MinuteReloadTime = (60/((MagDumpTime)+CalcTable[6]))
 	local DamagePerMagCalc, EffectCalc, DamagePerSecondCalc = (CalcTable[1]*MagDumpTime*CalcTable[4]*CalcTable[5]), 0, (CalcTable[1]/CalcTable[3])*CalcTable[4]*CalcTable[5]
 	local TimeSpentShootingCalc, TimeSpentReloadingCalc = MinuteReloadTime*MagDumpTime, MinuteReloadTime*CalcTable[6]
-	if (CalcTable[7] > 0) and (CalcTable[8] > 0) then EffectCalc = (CalcTable[7]/CalcTable[8]) end
+	if pcall(tonumber(CalcTable[8])) and pcall(tonumber(CalcTable[7])) > 0 then EffectCalc = (CalcTable[7]/CalcTable[8]) end
 	if DamagePerMinute then table.insert(AnswersTable, ">>[OUTPUT]: Damage Per Minute: "..tostring(MinuteReloadTime*(DamagePerMagCalc+EffectCalc)).."\n") end
 	if DamagePerMag then table.insert(AnswersTable, ">>[OUTPUT]: Damage Per Mag: "..tostring(DamagePerMagCalc).."\n") end
 	if DamagePerSecond then table.insert(AnswersTable, ">>[OUTPUT]: Damage Per Second: "..tostring(DamagePerSecondCalc).."\n") end
