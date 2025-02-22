7617141777fc0dde7cca1fe22c9c6d5daf6a758c
LiKTOi1029 <testingboy5689@gmail.com>
2025-02-22 12:33:49 +0330

    readme.md adding

--- 2025/February 22/monster.bat
+++ 2025/February 22/monster.bat
@@ -2,7 +2,10 @@
 setlocal
 set Monetary=abdul
 set Money=dinar
+set Money[0]=dollar
+set Money[1]=dirham
 set Mon
 > con echo %Mon%
+endlocal
 pause
 cls
---
+++ 2025/February 22/readme.md
@@ -0,0 +1,3 @@
+# Monster.bat
+
+This is just me starting up my appetite by writing a table and also writing some variables. Nothing too special
