29d9371ae79c0b5d3a26131e8e3878817c55f8e6
LiKTOi1029 <testingboy5689@gmail.com>
2025-02-24 16:30:37 +0330

    collatzconjecture.bat saving progress

--- 2025 Batch/February 24/CollatzConjecture.bat
+++ 2025 Batch/February 24/CollatzConjecture.bat
@@ -1,18 +1,9 @@
 @echo off
-setlocal EnableExtensions EnableDelayedExpansion
+setlocal EnableExtensions
 set /a Number=0
+set /a Input=%1
+set /a Input=%Input%/2
 :start
-	if "%Input%" equ "" (
-  set /a Input=%1
-  set /a InputDivided=%Input%/2
-  ) else if InputDivided equ 1 (
-  set /a Input=(%Input%*3+1)
-  ) else if InputDivided equ 0 (
-  set /a Input=%Input%/2
-  ) else (
-  > con echo %InputDivided% not working
-  goto end
-  )
   set /a Number=Number+1
   if Input equ 1 (goto end) else (goto start)
 :end
