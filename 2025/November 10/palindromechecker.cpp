bccd064d086f7af30283451fcf8d9b8ebcd256e4
LiKTOi1029 <testingboy5689@gmail.com>
2025-11-10 11:14:09 +0330

    palindromechecker.cpp adding PalindromeChecker string return type function

--- 2025/November 10/palindromechecker.cpp
+++ 2025/November 10/palindromechecker.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
+#include <string>
+#include <list>
+
+using namespace std;
+
+string PalindromeCheckerFunction(string Original_Text)
+{
+	list<char> Full_Char_List;
+	int Length = Original_Text.length();
+	cout << Length << endl;
+	for (int i = 0; i < Length; i++)
+	{
+		char Character = Original_Text[i];
+		cout << Character << endl;
+	}
+	return "";
+}
 
 int main()
 {
-	return 0;
+	string Original_Text;
+	getline (cin, Original_Text);
+	string New_Text = PalindromeCheckerFunction(Original_Text);
 }
+
+// this is not finished lol. I am only just getting started.
