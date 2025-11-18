d586881b1f91c02f9d2900d73a94192fc1158909
LiKTOi1029 <testingboy5689@gmail.com>
2025-11-18 08:59:30 +0330

    palindromechecker.cpp finishing project

--- 2025/November 10/palindromechecker.cpp
+++ 2025/November 10/palindromechecker.cpp
@@ -17,22 +17,30 @@ vector<char> Swapper(vector<char> Vector, int Index_One, int Index_Two)
 string PalindromeCheckerFunction(string Original_Text)
 {
 	vector<char> Full_Vector(0);
+	vector<char> Full_Vector_New(0);
+	int Size;
 	int Length = Original_Text.length();
 	string Result;
 	cout << Length << endl;
 	for (int i = 0; i < Length; i++)
 	{
 		char Character = Original_Text[i];
-		cout << 1;
 		Full_Vector.push_back(Character);
-		cout << 2;
-		cout << Full_Vector[i] << endl;
 	}
+	Size = Full_Vector.size()-1;
+	cout << Size << endl;
 	for (int i = 0; i < ceil(Length/2); i++)
 	{
-		
+		Size = (Size-i);
+		Full_Vector_New = Swapper(Full_Vector, i, Size);
+		cout << Full_Vector[i] << " " << Full_Vector[Size] << endl;
 	}
-	return "";
+	string New_Text(Full_Vector_New.begin(), Full_Vector_New.end());
+	if (New_Text == Original_Text)
+	{
+		return New_Text + " and " + Original_Text + " Are palindromes!";
+	}
+	return New_Text + " and " + Original_Text + " Are not palindromes!";
 }
 
 int main()
@@ -40,6 +48,7 @@ int main()
 	string Original_Text;
 	getline (cin, Original_Text);
 	string New_Text = PalindromeCheckerFunction(Original_Text);
+	cout << New_Text << endl;
 }
 
 // this is not finished lol. I am only just getting started.
