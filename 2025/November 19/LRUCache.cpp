1ddfebc9662ff9e0906da5fc42e805ff0a63236f
LiKTOi1029 <testingboy5689@gmail.com>
2025-11-19 09:41:38 +0330

    LRUCache.cpp adding std::string LowercaseFunction(). updating int main().

---
+++ 2025/November 19/LRUCache.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <locale>
+
+std::string LowercaseFunction(std::string Input;)
+{
+	std::locale Locale;
+	std::string Result;
+	for (std::string::size_type i=0; i < Input.max_size(); ++i)
+	{
+		std::tolower(Input[i],Locale);
+	}
+	std::cout << Input << std::endl;
+	return Input;
+}
+int main()
+{
+	std::vector<int,5> = LRUCache;
+	std::string Choice;
+	std::cout << "Input an integer to add to the Cache, current cache items: \n" <<
+	for (auto item = LRUCache.begin(); item != LRUCache.end(); ++i)
+	{
+		std::cout << *item << " ";
+	}
+	std::cout << std::endl;
+	std::cout << "A) Add a number\nU) Use a number\nEXIT) Exit the script\n";
+	std::cin >> Choice;
+	if (Choice == "A") 
+	{
+		if (LRUCache.max_size() == LRUCache.size()) 
+		{
+			
+		}
+		else 
+		{
+			
+		}
+	}
+}
